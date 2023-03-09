3990ed82d9e13a66ddfb042703eaace844c51cf8
Antonio Ricardo <antonio.eschola@gmail.com>
2023-03-08 21:58:25 -0500

    feat: add the second layer prompt

--- main.go
+++ main.go
@@ -5,15 +5,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/arferreira/mentan-blackbox/openai"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
-
-
-
 
 type EbookInfoProduct struct {
     Title         string `json:"title"`
@@ -23,7 +21,6 @@ type EbookInfoProduct struct {
     Format        string `json:"format"`
 }
 
-
 type ebookData struct {
     Title           string `json:"title"`
     Niche           string `json:"niche"`
@@ -32,13 +29,47 @@ type ebookData struct {
 }
 
 
+// getChapters generates ebook chapters using OpenAI API
+func getChapters(ebook EbookInfoProduct) (string, error){
+    chaptersPrompt := fmt.Sprintf("Create 10 chapters for an ebook for me about %s with this %s niche using correct grammar and engaged words. Give me separated by comon", ebook.Title, ebook.Niche)
+    chapters := openai.OpenAiPrompt{
+        Prompt:      chaptersPrompt,
+        Temperature: 1.0,
+        MaxTokens:   10,
+    }
+
+    response, err := openai.SecondLayer(chapters.Prompt)
+    if err != nil {
+        return "", fmt.Errorf("failed to generate ebook content: %v", err)
+    }
+    return response, nil
+}
+
+func getDescription(ebook EbookInfoProduct) (string, error) {
+    descriptionPrompt := fmt.Sprintf("Create a description for an ebook for me about %s with this %s niche using correct grammar and engaged words.", ebook.Title, ebook.Niche)
+
+    description := openai.OpenAiPrompt{
+        Prompt:      descriptionPrompt,
+        Temperature: 1.0,
+        MaxTokens:   10,
+    }
+
+    response, err := openai.SecondLayer(description.Prompt)
+    if err != nil {
+        return "", fmt.Errorf("failed to generate ebook content: %v", err)
+    }
+    return response, nil
+}
+
 func createEbook(c *gin.Context) {
-    fmt.Println(c.Request.Body)
+    startTime := time.Now()
 
     var data ebookData
+    log.Printf("data: %v", data)
 
     if err := c.ShouldBindJSON(&data); err != nil {
         c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        fmt.Println("Err: ", err)
         return
     }
 
@@ -52,48 +83,44 @@ func createEbook(c *gin.Context) {
     log.Printf("Organization ID: %s", organizationID)
     log.Printf("Product ID: %s", productID)
 
-
-
-
     ebook := EbookInfoProduct{
         Title:         title,
-        Niche: niche,
+        Niche:         niche,
         Organization: organizationID,
         Product:       productID,
         Format:        "ebook",
     }
 
+    description, err := getDescription(ebook)
 
-    prompt := fmt.Sprintf("Create a ebook for me about %s with this %s niche using correct grammar and engaged content. I want it with introduction, 10 chapters and conclusion.", ebook.Title, ebook.Niche)
-
-    secondLayer := openai.OpenAiPrompt{
-        Prompt:      prompt,
-        Temperature: 1.0,
-        MaxTokens:   10,
-    }
-
-    response, err := openai.SecondLayer(secondLayer.Prompt)
     if err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate ebook content"})
-        fmt.Fprintf(os.Stderr, "failed to generate ebook content: %v\n", err)
+        fmt.Fprintf(os.Stderr, "%v\n", err)
         return
     }
-    c.JSON(http.StatusOK, gin.H{"data": response})
+
+    chapters, err := getChapters(ebook)
+
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate ebook content"})
+        fmt.Fprintf(os.Stderr, "%v\n", err)
+        return
+    }
+    fmt.Println("Chapters generated", chapters)
+    c.JSON(http.StatusOK, gin.H{"data": description})
+
+    elapsedSeconds := time.Since(startTime).Seconds()
+    log.Printf("Elapsed time for the first layer: %f seconds", elapsedSeconds)
 }
 
-
-
-
 func main() {
-
     // load .env file
     err := godotenv.Load()
     if err != nil {
-    log.Fatal("Error loading .env file")
+        log.Fatal("Error loading .env file")
     }
 
-
-	router := gin.Default()
+    router := gin.Default()
     router.Use(gin.Logger())
 
     // Configure CORS middlewares
@@ -101,16 +128,15 @@ func main() {
     config.AllowOrigins = []string{"http://localhost:3000"}
     router.Use(cors.New(config))
 
-
     // root path
-	router.GET("/", func(c *gin.Context) {
+    router.GET("/", func(c *gin.Context) {
         c.String(200, "The mentan blackbox is running...")
     })
 
     // generate ebook route
     router.POST("/api/v1/blackbox/ebook", createEbook)
 
-	if err := router.Run(":8000"); err != nil {
+    if err := router.Run(":8000"); err != nil {
         fmt.Fprintf(os.Stderr, "server failed: %v\n", err)
         os.Exit(1)
     }
